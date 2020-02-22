1abbe781d9fac33b9685cd874489952b0e9c076d
Yu Song <ysong17@stlawu.edu>
2020-02-22 17:09:24 -0500

    Mongodb schema

---
+++ A2/schema.js
@@ -0,0 +1,18 @@
+
+const mongoose = require('mongoose');
+
+const Voter = new mongoose.Schema({
+
+  firstName: String,
+  lastName: String,
+  zipCode: Number,
+  historyString: String
+
+});
+
+Voter.index({firstName: 1})
+Voter.index({lastName: 1});
+Voter.index({zipCode: 1});
+Voter.index({historyString: 1});
+
+module.exports = mongoose.model('Voter', Voter);
