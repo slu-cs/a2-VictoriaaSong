17ec366682c21339304149875cb23b39523dfd7d
Victoria Song <songyu@VictoriadeMacBook-Pro.local>
2020-02-23 14:24:23 -0500

    Close connection after promises

--- A2/query.js
+++ A2/query.js
@@ -10,19 +10,19 @@ connect();  // To the database
 const queries = [
 
   // How many registered voters live in the Canton zip code (13617)?
-  Voter.find().where('zipCode').equals(13617),
+  Voter.find().where('zipCode').equals(13617).count(),
 
   // What are the full names of all the registered voters whose first-name is STARR?
   Voter.find().where('firstName').equals('STARR'),
 
   // How many people voted in the 2016 general election (GE16)?
-  Voter.find().where('firstName').in('GE16'),
+  Voter.find().where('firstName').in('GE16').count(),
 
   // What is the last-name that comes last in the county in alphabetical order?
   Voter.find().sort('-name').limit(1),
 
   // How many zip codes does the county contain?
-  Voter.distinct('zipCode')
+  Voter.distinct('zipCode').count()
 
 
 ];
@@ -30,10 +30,10 @@ const queries = [
 // Run the queries in parallel
 Promise.all(queries)
   .then(function(results) {
-    console.log('Number of registed voters live in Canton (zip code: 13617): ', results[0].length);
+    console.log('Number of registed voters live in Canton (zip code: 13617): ', results[0]);
     console.log('Full names of all the registered voters whose first-name is STARR: ', results[1].map(p => p.name));
-    console.log('Number of people voted in the 2016 general election (GE16): ', results[2].length;
+    console.log('Number of people voted in the 2016 general election (GE16): ', results[2]);
     console.log('The last-name that comes last in the county in alphabetical order: ', results[3].map(p => p.name));
-    console.log('Number of zip codes does the county contain: ', results[4].length);
+    console.log('Number of zip codes does the county contain: ', results[4]);
     mongoose.connection.close();
  }).catch(error => console.error(error.stack));
