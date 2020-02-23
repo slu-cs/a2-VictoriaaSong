202871bd97353ca7e83ff9108ef86dc47216b649
Victoria Song <songyu@VictoriadeMacBook-Pro.local>
2020-02-23 14:31:59 -0500

    Close connection after promises

--- A2/query.js
+++ A2/query.js
@@ -2,14 +2,9 @@
 
 const mongoose = require('mongoose');
 //const connect = require('./voters.csv');
-const fs = require('fs');
-const readline = require('readline');
-const file = readline.createInterface({
-  input: fs.createReadStream('majors.csv')
-});
 const Voter = require('./schema');
 
-connect();  // To the database
+//connect();  // To the database
 
 
 const queries = [
