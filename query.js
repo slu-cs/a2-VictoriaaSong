c7e523272e51dd2cbadc730335fcb0c425b01509
Victoria Song <songyu@VictoriadeMacBook-Pro.local>
2020-02-23 18:07:33 -0500

    update

--- query.js
+++ query.js
@@ -27,16 +27,16 @@ const queries = [
   Voter.find().sort('-name').limit(1),
 
   // How many zip codes does the county contain?
-  Voter.distinct('zipCode').count()
+  Voter.distinct('zipCode')
 ];
 
 // Run the queries in parallel
 Promise.all(queries)
   .then(function(results) {
-    console.log('Number of registed voters live in Canton (zip code: 13617): ', results[0].length());
+    console.log('Number of registed voters live in Canton (zip code: 13617): ', results[0].length);
     console.log('Full names of all the registered voters whose first-name is STARR: ', results[1].map(p => p.firstName), results[1].map(p => p.lastName));
-    console.log('Number of people voted in the 2016 general election (GE16): ', results[2].length());
+    console.log('Number of people voted in the 2016 general election (GE16): ', results[2].length);
     console.log('The last-name that comes last in the county in alphabetical order: ', results[3].map(p => p.lastName));
-    console.log('Number of zip codes does the county contain: ', results[4]);
+    console.log('Number of zip codes does the county contain: ', results[4].length);
     mongoose.connection.close();
  }).catch(error => console.error(error.stack));
