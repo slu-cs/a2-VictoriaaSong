d9f953b8bedbadb4a71803875c5f210b11a94371
Victoria Song <songyu@VictoriadeMacBook-Pro.local>
2020-02-23 16:57:35 -0500

    update

--- db.js
+++ db.js
@@ -11,7 +11,7 @@ module.exports = function() {
   mongoose.set('useFindAndModify', false);
 
   // Start connecting
-  mongoose.connect('mongodb://localhost');
+  mongoose.connect('mongodb://localhost/faculty');
 
   // Log errors
   mongoose.connection.on('error', function(error) {
