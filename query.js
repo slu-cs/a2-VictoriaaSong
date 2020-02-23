079c278712f22f918593b91977e1b92ea830ade1
Victoria Song <songyu@VictoriadeMacBook-Pro.local>
2020-02-23 18:02:51 -0500

    update

--- query.js
+++ query.js
@@ -37,6 +37,6 @@ Promise.all(queries)
     console.log('Full names of all the registered voters whose first-name is STARR: ', results[1].map(p => p.firstName), results[1].map(p => p.lastName));
     console.log('Number of people voted in the 2016 general election (GE16): ', results[2].length());
     console.log('The last-name that comes last in the county in alphabetical order: ', results[3].map(p => p.lastName));
-    console.log('Number of zip codes does the county contain: ', results[4];
+    console.log('Number of zip codes does the county contain: ', results[4]);
     mongoose.connection.close();
  }).catch(error => console.error(error.stack));
