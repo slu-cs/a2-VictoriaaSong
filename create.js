1ebc6b1056cfd62cdb4c03bb5fd418d92ef8cceb
Victoria Song <songyu@VictoriadeMacBook-Pro.local>
2020-02-23 16:45:54 -0500

    update

--- create.js
+++ create.js
@@ -18,20 +18,26 @@ const voters = [];
 
 file.on('line', function(line) {
   const columns = line.split(',');
-  const eachVoter = new Voter({
-    firstName: columns[0],
-    lastName: columns[1],
-    zipCode: Number(columns[2]),
-    historyString: columns[3]
-  });
-  voters.push(eachVoter);
+  // const eachVoter = new Voter({
+  //   firstName: columns[0],
+  //   lastName: columns[1],
+  //   zipCode: Number(columns[2]),
+  //   historyString: columns[3]
+  // });
+  voters.push(
+    new Voter({
+      firstName: columns[0],
+      lastName: columns[1],
+      zipCode: Number(columns[2]),
+      historyString: columns[3]
+    });
+  );
 });
 
 file.on('close', function(){
   mongoose.connection.dropDatabase()
 
     .then(() => Promise.all(voters.map(d => d.save())))
-
     .then(() => mongoose.connection.close())
     .then(() => console.log('Database is ready.'))
     .catch(error => console.error(error.stack));
